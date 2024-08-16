80cc4a55790701883c8fe81bae5ae19b1287cac5
Aaveshshah <112115211+Aaveshshah@users.noreply.github.com>
2024-08-16 16:28:10 +0530

    Add files via upload

---
+++ portfolio/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['<i>Java Developer</i>', ' <i>Front End Developer</i>', ' <i>Competitive Programmer</i>'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1500,
+    loop:true
+});
